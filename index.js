6cc7d97db58a336c8d2aab0b2945a8f7934552f2
Cartoon72 <tdesai5091@gmail.com>
2024-01-30 21:29:35 +0530

    added function like: random number generator and render game

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-let firstCard = 9
-let secondCard = 21
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards= [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBJ = false
 let isalive = true
 let message =""
+let messageEl = document.getElementById("message-el")
+let SumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    renderGame()
+}
+function renderGame(){
     if(sum<21){
-        message="Do you want to draw a new card? 🙂"
+        message="Do you want to draw a new card?"
     }
     else if(sum===21){
-        message="Wohoo! You've got Blackjack! 🥳"
+        message="Wohoo! You've got Blackjack!"
         hasBJ = true
     }
     else{
-        message="You're out of the game! 😭"
+        message="You're out of the game!"
         isalive = false    
     } 
-    
-    console.log(message)
+    SumEl.textContent = "Sum: "+ sum
+    cardsEl.textContent = "Cards: "
+    for(let i =0;i<cards.length;i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    messageEl.textContent = message
 }
 
+function newCard(){
+ let newCard = getRandomCard()
+ sum += newCard
+ cards.push(newCard)
+ renderGame( )
+}
+
+
+function getRandomCard(){
+    return Math.floor(Math.random()*13+1)
+}
