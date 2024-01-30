01e52ee19be89301360d464d24620c56421a3a55
Cartoon72 <tdesai5091@gmail.com>
2024-01-30 18:56:41 +0530

    Created the basic webpage with button

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-let i =10
+let firstCard = 9
+let secondCard = 21
+let sum = firstCard + secondCard
+let hasBJ = false
+let isalive = true
+let message =""
+
+function startGame(){
+    if(sum<21){
+        message="Do you want to draw a new card? 🙂"
+    }
+    else if(sum===21){
+        message="Wohoo! You've got Blackjack! 🥳"
+        hasBJ = true
+    }
+    else{
+        message="You're out of the game! 😭"
+        isalive = false    
+    } 
+    
+    console.log(message)
+}
+
